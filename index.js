29a5dd2580f683b534267840322307a24fcea01a
pratik88-tech <51715153+pratik88-tech@users.noreply.github.com>
2024-02-25 13:27:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app=express()
+const port=9000;
+
+app.use("/",(req,res)=>{
+    res.json({message:"Hello from Express App"})
+})
+app.listen(9000,()=>{
+    console.log("Starting server on port "+port)
+})
