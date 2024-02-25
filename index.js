b8e0b6e88c29077167fc8299a871aa78607f95f6
pratik88-tech <51715153+pratik88-tech@users.noreply.github.com>
2024-02-25 13:50:33 +0530

    cors added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express";
-
+import cors from "cors"
 const app=express()
 const port=9000;
-
+app.use(cors())
 app.use("/",(req,res)=>{
     res.json({message:"Hello from Express App"})
 })
